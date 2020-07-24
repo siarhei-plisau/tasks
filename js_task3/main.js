81a5f59f3d07dfeeaafca4431e65f0863babfc2a
Плисов Сергей <SPlisov@iggp.local>
2020-07-24 10:24:14 +0300

    fix some mistakes

--- js_task3/main.js
+++ js_task3/main.js
@@ -1,9 +1,10 @@
 function isObject(obj) {
-  if ( obj === null ) return false;
+  if ( obj === null || Object.prototype.toString.call(obj) === '[object Array]') return false;
   return typeof obj === 'object'
 }
 console.log('result isObject', isObject({}) );
 console.log('result isObject', isObject(null) );
+console.log('result isObject', isObject([]) );
 
 
 
@@ -16,10 +17,7 @@ console.log('result isFunction', isFunction(undefined) );
 
 // Аналог Object.keys(obj)
 function keys(obj) {
-  if ( !isObject(obj) ) {
-    console.error('variable is not object');
-    return;
-  }
+  if ( !isObject(obj) )  throw new Error('value is not object');
   const resultArray = [];
   for (const key in obj) {
     if (obj.hasOwnProperty(key)) resultArray.push(key);
@@ -29,15 +27,12 @@ function keys(obj) {
 const obj1 = {c: '3', d:'4' };
 const obj2 = {a: '1', b:'2', __proto__: obj1};
 console.log('result keys', keys(obj2));
-console.log('result keys', keys('2'));
+// console.log('result keys', keys('2'));
 
 
 
 function allKeys(obj) {
-  if ( !isObject(obj) ) {
-    console.error('variable is not object');
-    return;
-  }
+  if ( !isObject(obj) )  throw new Error('value is not object');
   const resultArray = [];
   for (const key in obj) {
     resultArray.push(key);
@@ -50,10 +45,7 @@ console.log('result allKeys', allKeys(obj2) );
 
 // Аналог Object.values(obj)
 function value(obj) {
-  if ( !isObject(obj) ) {
-    console.error('variable is not object');
-    return;
-  }
+  if ( !isObject(obj) )  throw new Error('value is not object');
   const resultArray = [];
   for (const key in obj) {
     if (obj.hasOwnProperty(key)) resultArray.push(obj[key]);
@@ -61,15 +53,12 @@ function value(obj) {
   return resultArray;
 }
 console.log('result value', value(obj2) );
-console.log('result value', value('5') );
+// console.log('result value', value('5') );
 
 
 // Аналог Object.entries(obj)
 function pair(obj) {
-  if ( !isObject(obj) ) {
-    console.error('variable is not object');
-    return;
-  }
+  if ( !isObject(obj) )  throw new Error('value is not object');
   const resultArray = [];
   for (const key in obj) {
     if (obj.hasOwnProperty(key))  resultArray.push([key, obj[key]]);
@@ -80,16 +69,14 @@ console.log('result pair', pair(obj1) );
 
 
 function functions(obj) {
-  if ( !isObject(obj) ) {
-    console.error('variable is not object');
-    return;
-  }
+  if ( !isObject(obj) )  throw new Error('value is not object');
   const resultArray = [];
   for (const key in obj) {
     if ( obj.hasOwnProperty(key) && isFunction(obj[key]) ) resultArray.push(obj[key].name);
   }
   return resultArray.sort();
 }
+
 const obj3 = {
   surname: 'Ivanov',
   name: 'Viktor',
@@ -99,13 +86,13 @@ const obj3 = {
 };
 console.log('result functions', functions(obj3) );
 
+ function bind(func, context) {
+  const bindArgArray = value(arguments).slice(2);
 
-
-function bind(func, context, arg) {
-  const argArray = value(arguments).slice(2);
-  return function() { 
-    return func.apply(context, argArray);
-  }
+  return function() {
+          const funcArg = value(arguments);
+          return func.apply(context, bindArgArray.concat(funcArg));
+          }
 }
- const functionAfterBind = bind(obj3.getName, obj3, '20 years', '2000');
- console.log( functionAfterBind() );
+console.log('result Bind:', bind(''.slice, '12345')(1, 3) ); 
+console.log('result Bind:',  bind(obj3.getName, obj3, '20 years', '2000')() );
